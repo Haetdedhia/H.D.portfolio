7301783284870b9019b56367a6cce9b435f60e39
Haet_dedhia <140299673+Haetdedhia@users.noreply.github.com>
2025-07-03 21:57:03 +0530

    Add files via upload

---
+++ assets/images/splash/splash.js
@@ -0,0 +1,23 @@
+const splashTexts = [
+  "50 Shades of Me",
+  "Autograph? Maybe later.",
+  "Certified Awesome™",
+  "Pixel-perfect Personality",
+  "Made of 100% Me",
+  "The Main Character",
+  "Crafted by Legends (aka me)",
+  "Witty. Pretty. Gritty.",
+  "Built different ",
+  "Loading brilliance...",
+  "Can’t spell ‘me’ without ME!",
+  "Your new obsession",
+  "Look, I sparkle ",
+  "Hotter than lava",
+  "They call me the End Boss",
+  "Epic loot: my personality",
+  "Powered by ego and espresso",
+  "NPCs fear me",
+  "Crafted in Hardcore Mode",
+  "What would Steve do?",
+  "Punching life like a tree"
+];
